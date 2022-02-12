a3288f8459126ef551353c6c8a93733c2b1103f9
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2022-02-12 14:18:27 -0800

    switched to new motor declarations for future odometry
    Co-Authored-By: Matt Pizzimenti <55747+mjpizz@users.noreply.github.com>
    Co-Authored-By: stephanyud2005 <55634293+stephanyud2005@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,35 +9,37 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import frc.robot.Constants;
 
 public class Drivetrain extends SubsystemBase {
 
-  protected DifferentialDrive differentialDrive;
+  TalonSRX driveRearLeftMotor = new TalonSRX(Constants.driveRearLeftCAN);
+  TalonSRX driveFrontLeftMotor = new TalonSRX(Constants.driveFrontLeftCAN);
+
+  TalonSRX driveRearRightMotor = new TalonSRX(Constants.driveRearRightCAN);
+  TalonSRX driveFrontRightMotor = new TalonSRX(Constants.driveFrontRightCAN);
 
   public Drivetrain() {
 
-    WPI_TalonSRX driveRearLeftMotor = new WPI_TalonSRX(Constants.driveRearLeftCAN);
-    WPI_TalonSRX driveFrontLeftMotor = new WPI_TalonSRX(Constants.driveFrontLeftCAN);
-    WPI_TalonSRX driveRearRightMotor = new WPI_TalonSRX(Constants.driveRearRightCAN);
-    WPI_TalonSRX driveFrontRightMotor = new WPI_TalonSRX(Constants.driveFrontRightCAN);
+    driveRearLeftMotor.setInverted(Constants.leftIsInverted);
+    driveFrontLeftMotor.setInverted(Constants.leftIsInverted);
+    driveRearLeftMotor.follow(driveFrontLeftMotor);
 
-    MotorControllerGroup leftMotors = new MotorControllerGroup(driveFrontLeftMotor, driveRearLeftMotor);
-    MotorControllerGroup rightMotors = new MotorControllerGroup(driveFrontRightMotor, driveRearRightMotor);
-    leftMotors.setInverted(true);
-    rightMotors.setInverted(false);
-    this.differentialDrive = new DifferentialDrive(leftMotors, rightMotors);
+    driveRearRightMotor.setInverted(Constants.rightIsInverted);
+    driveFrontRightMotor.setInverted(Constants.rightIsInverted);
+    driveRearRightMotor.follow(driveFrontRightMotor);
+
   }
 
-  public void tankDrive(double leftSpeed, double rightSpeed) {
+  public void tankDrive(double leftPercent, double rightPercent) {
     // TODO: consider filtering for smoother joystick driving
     // https://docs.wpilib.org/en/stable/docs/software/advanced-controls/filters/slew-rate-limiter.html
     // TODO: also could just try using the version of tankDrive that takes a 3rd argument true to decrease sensitivity at low speeds
-    this.differentialDrive.tankDrive(leftSpeed, rightSpeed);
+    driveFrontLeftMotor.set(TalonSRXControlMode.PercentOutput, leftPercent);
+    driveFrontRightMotor.set(TalonSRXControlMode.PercentOutput, rightPercent);    
   }
 
 }

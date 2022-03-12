d71770c6222e1f986718011c94f1ba7d680eb2dd
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2022-03-12 09:37:12 -0800

    updated constants to remove testing
    Co-Authored-By: Matt Pizzimenti <55747+mjpizz@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,42 +18,35 @@ public final class Constants {
   public static final int falconRearRightCAN = 2;
   public static final int falconFrontRightCAN = 3;
   public static final int pigeonCAN = 5;
-  // HACK: just testing out MotionMagic by co-opting the right-side drivetrain right now (falconArmCAN should be 6)
-  public static final int falconArmCAN = falconRearRightCAN;
-  public static final boolean leftFalconsAreInverted = true;
-  public static final boolean rightFalconsAreInverted = false;
-
-  //left and right Roller motors are inverted on the tuner
+  public static final int falconArmCAN = 6;
   public static final int leftRollerMotorCAN= 7;
   public static final int rightRollerMotorCAN = 8;
-  public static final int armSensorDIO = 0; 
-  public static final boolean leftRollerMotorIsInverted = true;
+  public static final boolean leftFalconsAreInverted = true;
+  public static final boolean rightFalconsAreInverted = false;
+  public static final boolean leftRollerMotorIsInverted = false;
   public static final boolean rightRollerMotorIsInverted = false;
-  public static final double drivetrainNeutralDeadbandPercentage = 0.08;
+
+  public static final int armSensorDIO = 0;
+
   // The Falcon 500s have a Talon FX Integrated sensor, which is rated for 2048 units per rotation:
   // https://docs.ctre-phoenix.com/en/latest/ch14_MCSensor.html#sensor-resolution
   public static final int sensorUnitsPerRevolution = 2048;
-
   public static final double driveWheelRadiusInInches = 3;
   public static final double trackWidthInInches = 22.0;
-  public static final double driveGearRatio = 7.31;
+  public static final double drivetrainNeutralDeadbandPercentage = 0.08;
+
   // From the System Identification for Connie
+  public static final double driveGearRatio = 7.31;
   public static final double kS = 0.62331;
   public static final double kV = 1.5593;
   public static final double kA = 0.20183;
   public static final double kP = 0.021079;
   public static final double kD = 0.0;
-  //public static final double driveGearRatio = 8.45;
 
-  // TODO: look into MotionMagic for the Arm
-  // https://docs.ctre-phoenix.com/en/latest/ch16_ClosedLoop.html#motion-magic-control-mode
-  // https://github.com/CrossTheRoadElec/Phoenix-Examples-Languages/blob/master/Java%20Talon%20FX%20(Falcon%20500)/MotionMagic/src/main/java/frc/robot/Robot.java
-  // public static final double armCruiseVelocity = 
-
+  // From the System Identification for Ponyo
+  // public static final double driveGearRatio = 8.45;
 
   public static final int driverGamepadPort = 0;
   public static final int operatorGamepadPort = 1;
-
-  public static final double maxNonBoostDrivePower = 0.3;
 
 }

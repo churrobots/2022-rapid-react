cea92a9d9e6f12807ada51ab81b992887037952b
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2022-03-12 09:38:45 -0800

    dead code
    Co-Authored-By: Matt Pizzimenti <55747+mjpizz@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/IntakeLeft.java
+++ src/main/java/frc/robot/subsystems/IntakeLeft.java
@@ -6,7 +6,6 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
-import edu.wpi.first.wpilibj.motorcontrol.PWMVictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 

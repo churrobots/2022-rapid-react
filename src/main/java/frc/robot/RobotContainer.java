712fa71ef678c8e568551bfed5528e4cd5003cdb
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2022-04-10 09:57:38 -0700

    make sure we get off the tarmac in 2-ball auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -153,7 +153,7 @@ public class RobotContainer {
         new AutoVacuum(muscleArm, polterLeftGust3000, polterRightGust3000), // drop arm and start intaking
         new AutoDriveWithSensorUnits(drivetrain, 32000, 32000), // drive to pickup
         new AutoReadyToScore(muscleArm, polterLeftGust3000, polterRightGust3000), // pull arm up
-        new AutoDriveWithSensorUnits(drivetrain, 38000, 38000), // get off the tarmac
+        new AutoDriveWithSensorUnits(drivetrain, 40000, 40000), // get off the tarmac
         new AutoDriveWithSensorUnits(drivetrain, 28000, 28000), // move back to prevent bumper crash
         new AutoDriveWithSensorUnits(drivetrain, 1924, 62000), // Sonic spin
         new ParallelRaceGroup(

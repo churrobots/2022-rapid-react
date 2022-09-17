c9cf0468c0538c7552381f398eb6f49cf648084c
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2022-09-17 12:39:43 -0700

    a bunch of lights stuff PAC MAN
    Co-Authored-By: stephanyud2005 <55634293+stephanyud2005@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/TheShow.java
+++ src/main/java/frc/robot/subsystems/TheShow.java
@@ -4,29 +4,97 @@
 
 package frc.robot.subsystems;
 
+import java.util.TimerTask;
+
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Drawings;
 
 public class TheShow extends SubsystemBase {
 
-  // NOTE: you can only allocate ONE strip of LEDs (in series). This is a limitation
+  // NOTE: you can only allocate ONE strip of LEDs (in series). This is a
+  // limitation
   // of how WPILib implements the addressable LED strips.
   // https://www.chiefdelphi.com/t/can-not-allocate-a-second-addressableled-pwm-port/376859
 
   // Our lights are set up as two 8x32 grids in series.
   AddressableLED leds = new AddressableLED(Constants.lightsPWM);
-  final int ROWS = 8;
-  final int COLS = 32;
-  final int STRIPS = 2;
-  final int PIXELS = (ROWS * COLS) * STRIPS;
-  AddressableLEDBuffer priorBuffer = new AddressableLEDBuffer(PIXELS);
+  final static int ROWS = 8;
+  final static int COLS = 32;
+  final static int STRIPS = 2;
+  final static int PIXELS = (ROWS * COLS) * STRIPS;
+  AddressableLEDBuffer emptyPixels = new AddressableLEDBuffer(PIXELS);
+
+  Timer t = new Timer();
+
+  static AddressableLEDBuffer wakaWakaPixels = new AddressableLEDBuffer(PIXELS);
+
+  private static void populateWakaWaka(int red, int green, int blue, int[][] wakawaka) {
+    int i = 0;
+    for (int c = 0; c < wakawaka.length; c++) {
+      if (c % 2 != 0) {
+        for (int r = 0; r < wakawaka[c].length; r++) {
+          if (wakawaka[c][r] == 1) {
+            wakaWakaPixels.setRGB(i, red, green, blue);
+          }
+          i++;
+        }
+      } else {
+        for (int r = wakawaka[c].length - 1; r >= 0; r--) {
+          if (wakawaka[c][r] == 1) {
+            wakaWakaPixels.setRGB(i, red, green, blue);
+          }
+          i++;
+        }
+      }
+    }
+  }
+
+  // private static void populateWakaWakaFull(int red, int green, int blue) {
+  //   // for (int i = 2; i < 6; i++) {
+  //   // wakaWakaPixels.setRGB(i, red, green, blue);
+  //   // }
+  //   // for (int i = 9; i < 15; i++) {
+  //   // wakaWakaPixels.setRGB(i, red, green, blue);
+  //   // }
+  //   // for (int i = 16; i < 48; i++) {
+  //   // wakaWakaPixels.setRGB(i, red, green, blue);
+  //   // }
+  //   // for (int i = 49; i < 55; i++) {
+  //   // wakaWakaPixels.setRGB(i, red, green, blue);
+  //   // }
+  //   // for (int i = 58; i < 62; i++) {
+  //   // wakaWakaPixels.setRGB(i, red, green, blue);
+  //   // }
+  //   int i = 0;
+  //   for (int c = 0; c < Drawings.mouth.length; c++) {
+  //     if (c % 2 != 0) {
+  //       for (int r = 0; r < Drawings.mouth[c].length; r++) {
+  //         if (Drawings.mouth[c][r] == 1) {
+  //           wakaWakaPixels.setRGB(i, red, green, blue);
+  //         }
+  //         i++;
+  //       }
+  //     } else {
+  //       for (int r = Drawings.mouth[c].length - 1; r >= 0; r--) {
+  //         if (Drawings.mouth[c][r] == 1) {
+  //           wakaWakaPixels.setRGB(i, red, green, blue);
+  //         }
+  //         i++;
+  //       }
+  //     }
+  //   }
+  // }
 
   public TheShow() {
     leds.setLength(PIXELS);
     leds.start();
+    t.start();
+    // populateWakaWaka(100, 40, 0);
   }
 
   public int getPixelCount() {
@@ -34,7 +102,8 @@ public class TheShow extends SubsystemBase {
   }
 
   public void clearPixels() {
-    fill(0, 0, 0);
+    leds.setData(emptyPixels);
+    populateWakaWaka(0, 0, 0, Drawings.empty);
   }
 
   public void fill(int red, int green, int blue) {
@@ -45,14 +114,33 @@ public class TheShow extends SubsystemBase {
     leds.setData(pixels);
   }
 
+  public void wakaWaka(int[][] wakawaka) {
+    populateWakaWaka(100, 45, 0, wakawaka);
+    leds.setData(wakaWakaPixels);
+  }
+
+  public void wakaWakaButBetter() {
+    if (t.get() <= 0.5) {
+      //this.wakaWaka(Drawings.fullMouth);
+      this.wakaWaka(Drawings.circle);
+    } else if (t.get() <= 1) {
+      this.wakaWaka(Drawings.halfMouth);
+    } else if (t.get() <= 1.5) {
+      //this.wakaWaka(Drawings.circle);
+      this.wakaWaka(Drawings.fullMouth);
+    } else {
+      t.reset();
+      this.clearPixels();
+
+    }
+
+  }
+
   public void setPixels(AddressableLEDBuffer buffer) {
     boolean isError = buffer.getLength() != PIXELS;
     if (isError) {
       fill(255, 0, 0);
       return;
-    }
-    if (!priorBuffer.equals(buffer)) {
-      leds.setData(buffer);
     }
   }
 
@@ -64,6 +152,7 @@ public class TheShow extends SubsystemBase {
   }
 
   private void runDefaultLights() {
-    this.fill(255, 0, 0);
+    // this.wakaWaka(100, 45, 0);
+    this.wakaWakaButBetter();
   }
 }
